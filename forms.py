eccf7fdb606c090b3f8409fe505048f1c0fd6fc8
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2022-05-16 18:48:40 -0500

    added signup and login forms

--- forms.py
+++ forms.py
@@ -1,19 +1,22 @@
 import email
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField
+from wtforms import StringField, PasswordField, TextAreaField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Email, Length, Optional
 
 
-# class SignupForm(FlaskForm):
-#     """Form for adding new users."""
+class SignupForm(FlaskForm):
+    """Form for adding new users."""
 
-#     username = StringField('Username', validators=[DataRequired()])
-#     email = StringField('E-mail', validators=[DataRequired(), Email()])
-#     password = PasswordField('Password', validators=[Length(min=6)])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[Email(),DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    zip_code = StringField('Zip Code', validators=[Length(min=5,max=5,message='Please enter a valid zip code.')])
+    is_band = BooleanField('Is this a band page?')
 
 
-# class LoginForm(FlaskForm):
-#     """Login form."""
+class LoginForm(FlaskForm):
+    """Login form."""
 
-#     username = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[Length(min=6)])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
