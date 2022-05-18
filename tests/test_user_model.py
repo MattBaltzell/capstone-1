707a2d5a28ff73a725ca586d52ca5883b2b963ac
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2022-05-17 20:46:10 -0500

    user model tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -41,93 +41,95 @@ class UserModelTestCase(TestCase):
             username="testuser",
             password="HASHED_PASSWORD",
             is_band=False,
-            
+            image_url=User.image_url.default.arg,
+            header_image_url=User.header_image_url.default.arg,
+            city='TestCity',
+            state='TestState',
+            zip_code='12345'
         )
 
         db.session.add(u)
         db.session.commit()
         
-        # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
+        # User should have no posts, followers, following, likes, genres, instruments or songs
+        self.assertEqual(len(u.posts), 0)
         self.assertEqual(len(u.followers), 0)
+        self.assertEqual(len(u.following), 0)
+        self.assertEqual(len(u.likes), 0)
+        self.assertEqual(len(u.genres), 0)
+        self.assertEqual(len(u.instruments), 0)
+        self.assertEqual(len(u.songs), 0)
+        self.assertNotEqual(len(u.songs), 10)
         # User __repr__ method should work correctly
         self.assertEqual(u.__repr__(),f'<User #{u.id}: testuser, test@test.com>' )
-
-    def test_following_methods(self):
-        """Check if 'is_following' and 'is_followed_by' methods successfully detect when users are and are not being followed"""
-
-        u1 = User(
-            email="test1@test.com",
-            username="testuser1",
-            password="HASHED_PASSWORD"
-        )
-        u2 = User(
-            email="test2@test.com",
-            username="testuser2",
-            password="HASHED_PASSWORD"
-        )
-
-        db.session.add(u1)
-        db.session.add(u2)
-        db.session.commit()
-
-        f = Follows(
-            user_being_followed_id=u2.id, 
-            user_following_id=u1.id
-            )
-
-        db.session.add(f)
-        db.session.commit()
-
-        self.assertEqual(u1.is_following(u2), True)
-        self.assertEqual(u2.is_followed_by(u1), True)
-        self.assertNotEqual(u2.is_following(u1), True)
-        self.assertNotEqual(u1.is_followed_by(u2), True)
-
-
+        self.assertEqual(u.is_band, False)
+        self.assertEqual(u.image_url, "/static/images/default-pic.png")
+        self.assertEqual(u.header_image_url, "/static/images/default-header-pic.jpg")
+        self.assertEqual(u.city, "TestCity")
+        self.assertEqual(u.state, "TestState")
+        self.assertEqual(u.zip_code, "12345")
+   
     def test_signup_method(self):
         """Check if signup method returns user whengiven valid credentials"""
 
-        u3 = User.signup(
-            email="test3@test.com",
-            username="testuser3",
+        u = User.signup(
+            username="testuser",
+            email="test@test.com",
             password="HASHED_PASSWORD",
+            zip_code='36832',
+            is_band=False,
             image_url=User.image_url.default.arg
         )
 
-        db.session.add(u3)
+        db.session.add(u)
         db.session.commit()
 
-        self.assertEqual(u3.username, 'testuser3')
-        self.assertEqual(u3.image_url, '/static/images/default-pic.png')
-        self.assertEqual(u3.header_image_url, '/static/images/warbler-hero.jpg')
+        self.assertEqual(u.username, 'testuser')
+        self.assertEqual(u.email, 'test@test.com')
+        self.assertNotEqual(u.password, 'HASHED_PASSWORD')
+        self.assertFalse(u.is_band)
+        self.assertEqual(u.image_url, '/static/images/default-pic.png')
+        self.assertEqual(u.header_image_url, '/static/images/default-header-pic.jpg')
+        self.assertIsNone(u.city)
+        self.assertIsNone(u.state)
+        self.assertEqual(u.zip_code, '36832')
+      
 
 
     def test_authenticate_method(self):
         """Check if authenticate method returns user if valid username and pword are given. Fails to return user if invalid username or pword."""
          
-        u4 = User.signup(
-            email="test4@test.com",
-            username="testuser4",
-            password="HASHED_PASSWORD",
-            image_url="/static/images/default-pic.png"
+        u = User.signup(
+            username='testuser',
+            email='test@test.com',
+            password='HASHED_PASSWORD',
+            zip_code='36832',
+            is_band=True,
+            image_url=User.image_url.default.arg
         )
 
-        db.session.add(u4)
+        db.session.add(u)
         db.session.commit()
 
-        auth_user = User.authenticate("testuser4","HASHED_PASSWORD")
+        auth_user = User.authenticate('testuser','HASHED_PASSWORD')
        
-        self.assertEqual(auth_user, u4)
-        self.assertEqual(auth_user.username, "testuser4")
-        self.assertEqual(auth_user.email, "test4@test.com")
-        self.assertEqual(auth_user.image_url, "/static/images/default-pic.png")
+        self.assertEqual(auth_user, u)
+        self.assertEqual(auth_user.username, 'testuser')
+        self.assertEqual(auth_user.email, 'test@test.com')
+        self.assertNotEqual(auth_user.password, 'HASHED_PASSWORD')
+        self.assertTrue(auth_user.is_band)
+        self.assertEqual(auth_user.image_url, '/static/images/default-pic.png')
+        self.assertEqual(auth_user.header_image_url, '/static/images/default-header-pic.jpg')
+        self.assertIsNone(auth_user.city)
+        self.assertIsNone(auth_user.state)
+        self.assertEqual(auth_user.zip_code, '36832')
         
-        auth_fail_user1 = User.authenticate("testuser4","WRONG_PASSWORD")
-        auth_fail_user2 = User.authenticate("WRONG_USERNAME","HASHED_PASSWORD")
+        auth_fail_user1 = User.authenticate('testuser','WRONG_PASSWORD')
+        auth_fail_user2 = User.authenticate('WRONG_USERNAME','HASHED_PASSWORD')
         self.assertEqual(auth_fail_user1, False)
         self.assertEqual(auth_fail_user2, False)
-        
+
+
 
 
 
