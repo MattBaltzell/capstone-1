4eaba5ea4a842cb20e271b741642a639fd62867b
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2022-05-20 18:11:04 -0500

    add slimselect form for multiselection

--- static/src/app.js
+++ static/src/app.js
@@ -1,5 +1,7 @@
 const heroVid = document.getElementById('hero-vid')
 const body = document.querySelector('body')
+const radiusText = document.querySelector('.radius_text')
+const radiusSlider = document.querySelector('.radius_slider')
 
 // Slow home page bg video down
 if (heroVid){
@@ -13,3 +15,14 @@ function closeFlashedMsg(e){
     if (!e.target.classList.contains('flash-close')) return;
     e.target.closest('.flash-msg').outerHTML = '';
 }
+
+
+body.addEventListener('input', function(e){
+    if(!radiusSlider) return;
+    radiusText.textContent = radiusSlider.value
+}) 
+
+
+new SlimSelect({
+    select: '#multiple'
+  })
