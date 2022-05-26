13143e863a60b73ca218feb4cfd84ab13ec7f9df
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2022-05-26 11:52:59 -0500

    add user relationships tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -4,7 +4,7 @@
 
 import os
 from unittest import TestCase
-from models import db, User, Likes, Follows, Instrument, Genre, Post
+from models import db, User, Likes, Follows, Instrument, User_Instrument, Genre, User_Genre, Post
 
 os.environ['DATABASE_URL'] = "postgresql:///hook-find-musicians-test"
 
@@ -44,7 +44,7 @@ class UserModelTestCase(TestCase):
             profile_image=User.profile_image.default.arg,
             header_image=User.header_image.default.arg,
             city='TestCity',
-            state='TestState',
+            state='TS',
             zip_code='12345'
         )
 
@@ -66,7 +66,7 @@ class UserModelTestCase(TestCase):
         self.assertEqual(u.profile_image, "/static/uploads/default-pic.png")
         self.assertEqual(u.header_image, "/static/uploads/default-header-pic.jpg")
         self.assertEqual(u.city, "TestCity")
-        self.assertEqual(u.state, "TestState")
+        self.assertEqual(u.state, "TS")
         self.assertEqual(u.zip_code, "12345")
    
     def test_signup_method(self):
@@ -76,8 +76,11 @@ class UserModelTestCase(TestCase):
             username="testuser",
             email="test@test.com",
             password="HASHED_PASSWORD",
+            city="Test City",
+            state="TS",
             zip_code='36832',
             is_band=False,
+            header_image=User.header_image.default.arg,
             profile_image=User.profile_image.default.arg
         )
 
@@ -90,8 +93,10 @@ class UserModelTestCase(TestCase):
         self.assertFalse(u.is_band)
         self.assertEqual(u.profile_image, '/static/uploads/default-pic.png')
         self.assertEqual(u.header_image, '/static/uploads/default-header-pic.jpg')
-        self.assertIsNone(u.city)
-        self.assertIsNone(u.state)
+        self.assertIsNotNone(u.city)
+        self.assertEqual(u.city, "Test City")
+        self.assertIsNotNone(u.state)
+        self.assertEqual(u.state, "TS")
         self.assertEqual(u.zip_code, '36832')
       
 
@@ -103,9 +108,12 @@ class UserModelTestCase(TestCase):
             username='testuser',
             email='test@test.com',
             password='HASHED_PASSWORD',
+            city='Test City',
+            state='ST',
             zip_code='36832',
             is_band=True,
-            profile_image=User.profile_image.default.arg
+            profile_image=User.profile_image.default.arg,
+            header_image=User.header_image.default.arg
         )
 
         db.session.add(u)
@@ -120,8 +128,8 @@ class UserModelTestCase(TestCase):
         self.assertTrue(auth_user.is_band)
         self.assertEqual(auth_user.profile_image, '/static/uploads/default-pic.png')
         self.assertEqual(auth_user.header_image, '/static/uploads/default-header-pic.jpg')
-        self.assertIsNone(auth_user.city)
-        self.assertIsNone(auth_user.state)
+        self.assertIsNotNone(auth_user.city)
+        self.assertIsNotNone(auth_user.state)
         self.assertEqual(auth_user.zip_code, '36832')
         
         auth_fail_user1 = User.authenticate('testuser','WRONG_PASSWORD')
@@ -130,6 +138,44 @@ class UserModelTestCase(TestCase):
         self.assertEqual(auth_fail_user2, False)
 
 
+    def test_user_relationship(self):
+        """Test ."""
+         
+        u = User.signup(
+            username='testuser',
+            email='test@test.com',
+            password='HASHED_PASSWORD',
+            city='Test City',
+            state='ST',
+            zip_code='36832',
+            is_band=True,
+            profile_image=User.profile_image.default.arg,
+            header_image=User.header_image.default.arg
+        )
 
+        i = Instrument(name='Test Instrument')
+        g = Genre(name='Test Genre')
+        
+        db.session.add(u)
+        db.session.add(i)
+        db.session.add(g)
+        db.session.commit()
 
+        u_i = User_Instrument(user_id=u.id, instrument_id=i.id)
+        u_g = User_Genre(user_id=u.id, genre_id=g.id)
+        db.session.add(u_i)
+        db.session.add(u_g)
+        db.session.commit()
 
+        self.assertIsNotNone(u.instruments)
+        self.assertIn(i, u.instruments)
+        self.assertEqual(u.instruments[0].name,'Test Instrument')
+        self.assertIsNotNone(i.users)
+        self.assertEqual(i.users[0].username,'testuser')
+        
+        self.assertIsNotNone(u.genres)
+        self.assertIn(g, u.genres)
+        self.assertEqual(u.genres[0].name,'Test Genre')
+        self.assertIsNotNone(g.users)
+        self.assertEqual(g.users[0].username,'testuser')
+        
