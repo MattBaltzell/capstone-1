a9009eaf0b3a1fc285196c579aa0fac2d86c3eb1
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2022-06-07 01:30:47 -0500

    update seed file

--- seed.py
+++ seed.py
@@ -3,6 +3,7 @@ from app import db
 from multiselect_options import INSTRUMENT_CHOICES, GENRE_CHOICES
 from models import User, Instrument, User_Instrument, User_Genre, Genre, Follows
 
+db.drop_all()
 db.create_all()
 
 #CREATE INSTRUMENTS AND GENRES
@@ -19,13 +20,15 @@ db.session.commit()
 
 with open('generator/users.csv') as users:
     db.session.bulk_insert_mappings(User, DictReader(users))
+    db.session.commit()
 
 with open('generator/follows.csv') as follows:
     db.session.bulk_insert_mappings(Follows, DictReader(follows))
-
+    db.session.commit()
 
 with open('generator/users_instruments.csv') as users_instruments:
     db.session.bulk_insert_mappings(User_Instrument, DictReader(users_instruments))
+    db.session.commit()
 
 with open('generator/users_genres.csv') as users_genres:
     db.session.bulk_insert_mappings(User_Genre, DictReader(users_genres))
